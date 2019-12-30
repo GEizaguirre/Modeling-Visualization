2f2fb6579f582e9722d56885b068b9fa7e66c5d5
German Telmo Eizaguirre <gteizaguirre@gmail.com>
2019-12-30 22:42:15 +0100

    Rotating jewelry with arrows + pivot moved

---
+++ TousViewer/TousViewer/Assets/Scripts/MyFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFunctions : MonoBehaviour
+{
+	float resultado;
+	private Vector3 target;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float movimientoHorizontal = Input.GetAxis("Horizontal");
+    	float movimientoVertical = Input.GetAxis("Vertical");
+    	target = new Vector3(movimientoVertical, movimientoHorizontal, 0.0f);
+    	transform.RotateAround(transform.Find("ChangedPivot").position, target ,50*Time.deltaTime);
+       	//float movimientoHorizontal = Input.GetAxis("Horizontal");
+       	//float movimientoVertical = Input.GetAxis("Vertical");
+       	//transform.Rotate(movimientoVertical, movimientoHorizontal, 0);
+        
+    }
+}
